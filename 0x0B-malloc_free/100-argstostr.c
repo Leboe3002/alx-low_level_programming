72da405b0d837f5e3ae271d8912f7105f7ba1731
leboe3002 <og.leboe@gmail.com>
2022-10-04 16:30:05 -0700

    completed

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,41 +1,45 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * argstostr - main entry
- * @ac: input
- * @av: pointer array
- * return: 0
- * */
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
+ */
 char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, l = 0;
 	char *str;
+	int arg, byte, index, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+
+	for (arg = 0; arg < ac; arg++)
 	{
-		for (i = 0; av[i][n]; n++)
-			l++;
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
 	}
-	l += ac;
-	
-	str = malloc(sizeof(char) * l + 1);
+
+	str = malloc(sizeof(char) * size + 1);
+
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-	for (i = 0; av[i][n]; n++)
-	{
-		str[r] = av[i][n];
-		r++;
-	}
-	if (str[r] == '\0')
-	{
-	str[r++] = '\n';
-	}
-	}
-	return (str);
-		
 
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}
