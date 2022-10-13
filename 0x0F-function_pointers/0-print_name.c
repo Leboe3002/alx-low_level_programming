653a06d683be794ff7d373127eff5b06c1fdea90
leboe3002 <og.leboe@gmail.com>
2022-10-12 18:27:19 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+
+iinme - Function to call a function
+ *
+ * @name: Parameter 1
+ * @f: parameter function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		if (name != NULL)
+		{
+			(*f)(name);
+		}
+	}
+}clude "dog.h"
